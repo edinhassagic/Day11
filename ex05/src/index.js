b3feb2417d6faf0b8943a96556f4390f0f8d536a
edin hasagic <edinhassagic@gmail.com>
2021-03-19 00:59:13 +0100

    5

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+myNestedArray = [["Toblerone", 5], ["Milka", 3]];
+
+function myNestedFunction(arr) {
+
+    myNestedArray = [...arr];
+    return myNestedArray;
+}
+
+console.log(myNestedFunction(myNestedArray));
+module.exports = myNestedFunction;
