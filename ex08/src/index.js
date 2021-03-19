9b5ef3154362249fa22cffb83499d815a2055b72
edin hasagic <edinhassagic@gmail.com>
2021-03-19 01:03:46 +0100

    8

---
+++ ex08/src/index.js
@@ -0,0 +1,23 @@
+function myMutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+
+console.log(myMutation(["hello", "hey"]));
+console.log(myMutation(["hello", "Hello"]));
+console.log(myMutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+console.log(myMutation(["Mary", "Army"]));
+console.log(myMutation(["Mary", "Aarmy"]));
+console.log(myMutation(["Alien", "line"]));
+console.log(myMutation(["floor", "for"]));
+console.log(myMutation(["hello", "neo"]));
+console.log(myMutation(["voodoo", "no"]));
+console.log(myMutation(["ate", "date"]));
+console.log(myMutation(["Tiger", "Zebra"]));
+console.log(myMutation(["Noel", "Ole"]));
+
+module.exports = myMutation;
